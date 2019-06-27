20c6b0d64d1add0209ac9bb5e89f391cf608cc5d
Andrei Bunulu <bunulu@gmail.com>
2019-06-27 02:04:08 +0200

    feat(button): basic button component created

---
+++ src/components/Button.svelte
@@ -0,0 +1,107 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    import classNames from 'classnames'
+
+    const dispatch = createEventDispatcher()
+
+    /**
+     * Contains a URL or a URL fragment that the hyperlink points to.
+     * If this property is set, an anchor tag will be rendered.
+     * string
+     */
+    export let href = ''
+    /**
+     * Active state
+     * boolean
+     */
+    export let active = false
+
+    /**
+     * Disabled state
+     * boolean
+     */
+    export let disabled = false
+
+    /**
+     * Spans the full width of the parent component
+     * boolean
+     */
+    export let block = false
+
+    /**
+     * Button variant, default 'button'
+     * enum {"primary" | "secondary" | "success" | "danger"} = "primary"
+     */
+    export let variant = 'primary'
+
+    /**
+     * Button type, default 'button'
+     * enum {"button" | "reset" | "submit"} = "button"
+     */
+    export let type = 'button'
+
+    /**
+     * Button size
+     * enum {"small" | "normal" | "large"} = "normal"
+     */
+    export let size = 'normal'
+
+    let classList = classNames({ active, disabled, block, [variant]: true, [size]: true })
+
+    function handleClick() {
+        dispatch('click')
+    }
+</script>
+
+<style>
+    button {
+        border: 0;
+        border-radius: 5px;
+    }
+
+    .active {
+        background-color: aqua;
+    }
+
+    .primary {
+        background-color: aquamarine;
+    }
+
+    .secondary {
+        background-color: bisque;
+    }
+
+    .success {
+        background-color: darkgreen;
+    }
+
+    .danger {
+        background-color: red;
+    }
+
+    .block {
+        width: 100%;
+    }
+
+    .small {
+        font-size: 12px;
+    }
+
+    .normal {
+        font-size: 16px;
+    }
+
+    .large {
+        font-size: 24px;
+    }
+</style>
+
+{#if href}
+    <a {href} on:click class={classList}>
+        <slot />
+    </a>
+{:else}
+    <button on:click {type} class={classList} disabled>
+        <slot />
+    </button>
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,60 @@
+<script>
+    import Button from '../components/Button.svelte'
+
+    function handleClick() {
+        console.info('Clicked')
+    }
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+    h1,
+    figure,
+    p {
+        text-align: center;
+        margin: 0 auto;
+    }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    h1 {
+        font-size: 2.8em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    figure {
+        margin: 0 0 1em 0;
+    }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+    img {
+        width: 100%;
+        max-width: 400px;
+        margin: 0 0 1em 0;
+    }
 
-	p {
-		margin: 1em auto;
-	}
+    p {
+        margin: 1em auto;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+    <title>Money management</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
+<Button active disabled on:click={handleClick}>Test</Button>
+
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+    <img alt="Borat" src="great-success.png" />
+    <figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+    <strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong>
+</p>
